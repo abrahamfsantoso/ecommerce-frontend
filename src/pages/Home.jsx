ec60b63bda789c1c5eb07dfa52020c9fceb3d1e3
Abraham Fajar Santoso <abrahamfsantoso@gmail.com>
2021-09-05 22:40:34 +0700

    added navbar and announcement

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Announcement from '../components/Announcement';
 
 const Home = () => {
-    return (
-        <div>
-            Homepage
-        </div>
-    )
-}
+  return (
+    <div>
+      <Announcement />
+      <Navbar />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
